00cbdab708f025f1a660e695459c7ba927e2e443
LucaCorleto6 <lcorleto@mail.austral.edu.ar>
2026-04-28 12:37:25 -0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,16 +1,49 @@
 # Replace the "ANSWER HERE" for your answer
 
 def roots(a, b, c):
-    return "ANSWER HERE"
+    if (b**2 - 4*a*c) < 0:
+        return"( )"
+    elif (b**2 - 4*a*c) == 0:
+        return f"({-1*b/2*a})"
+    elif (b**2 - 4*a*c) > 0:
+        r1 = (-b + (b**2 - 4*a*c)**(1/2))/2*a
+        r2 = (-b - (b**2 - 4*a*c)**(1/2))/2*a
+        return f"({r1}, {r2})"
 
 
 def value_y(a, b, c, x):
-    return "ANSWER HERE"
+    y = a*x**2 + b*x + c
+    return y
 
 
 def to_string(a, b, c):
-    return "ANSWER HERE"
+    if a != 0 and b != 0 and c != 0:
+        return f"f(x) = {a} * X^2 + {b} * X + {c}"
+    elif a != 0 and b == 0 and c != 0:
+        return f"f(x) = {a} * X^2 + {c}"
+    elif a == 0 and b!= 0 and c != 0:
+        return f"f(x) = {b} * X + {c}"
+    elif a !=0 and b !=0 and c==0:
+        return f"f(x) = {a} * X^2 + {b} * X"
+    elif a != 0 and b == 0 and c == 0:
+        return f"f(x) = {a} * X^2"
+    elif a == 0 and b != 0 and c == 0:
+        return f"f(x) = {b} * X"
+    elif a == 0 and b == 0 and c != 0:
+        return f"f(x) = {c}"
+    elif a ==0 and b ==0 and c == 0:
+        return f"f(x) = 0"
+
+
+    return f"f(x) = {a} * X^2 + {b} * X + {c}"
 
 
 def derivation(a, b, c):
-    return "ANSWER HERE"
+    if a != 0 and b!= 0:
+        return f"f'(x) = {a * 2} * X + {b}"
+    elif a != 0 and b == 0:
+        return f"f'(x) = {a * 2} * X"
+    elif a == 0 and b!= 0:
+        return f"f'(x) = {b}"
+    elif a==0 and b==0:
+        return f"f'(x) = 0"
